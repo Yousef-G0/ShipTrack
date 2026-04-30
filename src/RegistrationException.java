27dacf9866d3cb16142d3b0285c4c223b9c5ebd3
Yousef-G0 <yfeery511@gmail.com>
2026-04-30 20:05:15 +0300

    Added:Description

--- src/RegistrationException.java
+++ src/RegistrationException.java
@@ -2,4 +2,4 @@ public class RegistrationException extends Exception {
     public RegistrationException(String message) {
         super(message);
     }
-}
+} // stops Registration Immediately if anything wrong happen.
